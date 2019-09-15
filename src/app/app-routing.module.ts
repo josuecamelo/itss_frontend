8c5d62ce7833ca2fbb15fdbdfe59b4e7f5ed1105
Josué Camelo <josueprg@gmail.com>
2019-09-14 23:42:55 -0300

    Listagem de Registros(Modelos, Cores, Pátios)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,15 @@ import {EditarUsuarioComponent} from './editar-usuario/editar-usuario.component'
 import {AdicionarUsuarioComponent} from './adicionar-usuario/adicionar-usuario.component';
 import {AppComponent} from './app.component';
 import {UsuariosComponent} from './usuarios/usuarios.component';
-
+import {ModelosComponent} from './modelos/modelos.component';
+import {EditarModeloComponent} from './editar-modelo/editar-modelo.component';
+import {AdicionarModeloComponent} from './adicionar-modelo/adicionar-modelo.component';
+import {CoresComponent} from './cores/cores.component';
+import {EditarCorComponent} from './editar-cor/editar-cor.component';
+import {AdicionarCorComponent} from './adicionar-cor/adicionar-cor.component';
+import {PatiosComponent} from './patios/patios.component';
+import {EditarPatioComponent} from './editar-patio/editar-patio.component';
+import {AdicionarPatioComponent} from './adicionar-patio/adicionar-patio.component';
 
 const routes: Routes =  [
   { path: 'clientes', component: ClientesComponent },
@@ -16,7 +24,16 @@ const routes: Routes =  [
   { path: 'usuarios', component: UsuariosComponent },
   { path: 'usuarios/editar', component: EditarUsuarioComponent },
   { path: 'usuarios/adicionar', component: AdicionarUsuarioComponent },
-  { path: '**', component: AppComponent }
+  { path: 'modelos', component: ModelosComponent },
+  { path: 'modelos/editar', component: EditarModeloComponent },
+  { path: 'modelos/adicionar', component: AdicionarModeloComponent },
+  { path: 'cores', component: CoresComponent },
+  { path: 'cores/editar', component: EditarCorComponent },
+  { path: 'cores/adicionar', component: AdicionarCorComponent },
+  { path: 'patios', component: PatiosComponent },
+  { path: 'patios/editar', component: EditarPatioComponent },
+  { path: 'patios/adicionar', component: AdicionarPatioComponent },
+  /*{ path: '**', component: AppComponent }*/
 ];
 
 @NgModule({
