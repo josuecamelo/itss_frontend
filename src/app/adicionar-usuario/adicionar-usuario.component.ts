72c485b8d6ae2c6759bf982244968fe20eac114c
Josué Camelo <josueprg@gmail.com>
2019-09-14 23:10:30 -0300

    Adicionar / Editar Usuários

--- src/app/adicionar-usuario/adicionar-usuario.component.ts
+++ src/app/adicionar-usuario/adicionar-usuario.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Message} from 'primeng/api';
+import {Router} from '@angular/router';
+import {ClientesService} from '../client/clientes/service/clientes.service';
+import {Usuario} from '../models/usuario';
+import {UsuariosService} from '../client/usuarios/service/usuarios.service';
 
 @Component({
   selector: 'app-adicionar-usuario',
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adicionar-usuario.component.css']
 })
 export class AdicionarUsuarioComponent implements OnInit {
+  addForm: FormGroup;
+  msgs: Message[] = [];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private usuarioService: UsuariosService) { }
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [''],
+      nome: ['', Validators.required],
+      login: ['', Validators.required],
+      senha: ['', Validators.required],
+      telefone: ['', Validators.required],
+    });
   }
 
+  onSubmit() {
+    this.usuarioService.create(this.addForm.value)
+      .subscribe( data => {
+        this.router.navigate(['/usuarios']);
+      });
+  }
 }
--- src/app/editar-usuario/editar-usuario.component.ts
+++ src/app/editar-usuario/editar-usuario.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Cliente} from '../models/cliente';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Message} from 'primeng/api';
+import {Router} from '@angular/router';
+import {ClientesService} from '../client/clientes/service/clientes.service';
+import {Usuario} from '../models/usuario';
+import {UsuariosService} from '../client/usuarios/service/usuarios.service';
 
 @Component({
   selector: 'app-editar-usuario',
@@ -7,9 +14,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarUsuarioComponent implements OnInit {
 
-  constructor() { }
+  usuarioId: any;
+  usuario: Usuario;
+  editForm: FormGroup;
+  msgs: Message[] = [];
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private router: Router, private usuarioService: UsuariosService) {
+
   }
 
+  ngOnInit() {
+    this.usuarioId = window.localStorage.getItem('editUsuarioId');
+    if (!this.usuarioId) {
+      alert('Ação Inválida.');
+      this.router.navigate(['usuarios']);
+      return;
+    }
+
+    this.editForm = this.formBuilder.group({
+      id: [''],
+      nome: ['', Validators.required],
+      login: ['', Validators.required],
+      senha: ['', Validators.required],
+      telefone: ['', Validators.required],
+    });
+
+    this.usuarioService.getUsuarioById(+this.usuarioId)
+      .subscribe( data => {
+        this.editForm.setValue(data);
+      });
+  }
+
+
+  onSubmit() {
+    this.usuarioService.update(this.editForm.value)
+      .subscribe(data => {
+          if (data.id) {
+            this.msgs = [];
+            this.msgs.push({ severity: 'success', summary: 'Sucesso', detail: `O Usuário foi Editado com Sucesso` });
+            this.router.navigate(['usuarios']);
+          } else {
+            this.msgs = [];
+            this.msgs.push({ severity: 'error', summary: 'Falha', detail: `O Usuário não pode ser Editado` });
+          }
+        },
+        error => {
+          alert(error);
+        });
+  }
 }
