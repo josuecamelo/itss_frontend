9625e5831b59cc450b4fbd567f72e1373e55cd82
Josué Camelo <josueprg@gmail.com>
2019-09-15 00:21:37 -0300

    Ajustes Listagem de Itens

--- src/app/cores/cores.component.ts
+++ src/app/cores/cores.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {CoresService} from '../client/cores/service/cores.service';
+import {Cor} from '../models/cor';
 
 @Component({
   selector: 'app-cores',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cores.component.css']
 })
 export class CoresComponent implements OnInit {
+  public cores: Cor[];
+  public cols: any[];
+  constructor(private router: Router, private corService: CoresService) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.getCores();
+    this.cols = [
+      { field: 'id', header: '#' },
+      { field: 'nome', header: 'Nome' },
+    ];
+  }
+
+  private getCores(): void {
+    this.corService.getCores().subscribe(data => {
+      this.cores = data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public recuperarRegistro(cor: Cor): void {
+    window.localStorage.removeItem('editCorId');
+    window.localStorage.setItem('editCorId', cor.id.toString());
+    this.router.navigate(['cores/editar']);
+  }
+
+  public excluir(cor: Cor): void {
+    this.corService.delete(cor.id)
+      .subscribe( data => {
+        const index = this.cores.indexOf(cor);
+        this.cores.splice(index, 1);
+      });
+  }
 }
--- src/app/patios/patios.component.ts
+++ src/app/patios/patios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {PatiosService} from '../client/patios/service/patios.service';
+import {Patio} from '../models/patio';
 
 @Component({
   selector: 'app-patios',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patios.component.css']
 })
 export class PatiosComponent implements OnInit {
+  public patios: Patio[];
+  public cols: any[];
+  constructor(private router: Router, private patioService: PatiosService) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.getPatio();
+    this.cols = [
+      { field: 'id', header: '#' },
+      { field: 'descricao', header: 'Descrição' },
+      { field: 'taxaHora', header: 'Valor/Hora' },
+    ];
+  }
+
+  private getPatio(): void {
+    this.patioService.getPatios().subscribe(data => {
+      this.patios = data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public recuperarRegistro(patio: Patio): void {
+    window.localStorage.removeItem('editPatioId');
+    window.localStorage.setItem('editPatioId', patio.id.toString());
+    this.router.navigate(['patios/editar']);
+  }
+
+  public excluir(patio: Patio): void {
+    this.patioService.delete(patio.id)
+      .subscribe( data => {
+        const index = this.patios.indexOf(patio);
+        this.patios.splice(index, 1);
+      });
+  }
 }
