751e0d4062837611e1af3a168a1acda17f42bdf7
Josué Camelo <josueprg@gmail.com>
2019-09-15 15:09:58 -0300

    Veiculos

--- src/app/adicionar-veiculo/adicionar-veiculo.component.ts
+++ src/app/adicionar-veiculo/adicionar-veiculo.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {Veiculo} from '../models/veiculo';
+import {Cor} from '../models/cor';
+import {Modelo} from '../models/modelo';
+import {Cliente} from '../models/cliente';
+import {Router} from '@angular/router';
+import {VeiculosService} from '../client/veiculos/service/veiculos.service';
+import {CoresService} from '../client/cores/service/cores.service';
+import {ClientesService} from '../client/clientes/service/clientes.service';
+import {ModeloService} from '../client/modelos/service/modelo.service';
 
 @Component({
   selector: 'app-adicionar-veiculo',
@@ -6,10 +15,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adicionar-veiculo.component.css']
 })
 export class AdicionarVeiculoComponent implements OnInit {
+  cores: Cor[];
+  modelos: Modelo[];
+  clientes: Cliente[];
 
-  constructor() { }
+  veiculo: Veiculo = {
+    id: null,
+    placa: null,
+    cliente: null,
+    modelo: null,
+    cor: null,
+    estacionadas: null
+  };
+
+  constructor( private router: Router, private veiculoService: VeiculosService,
+               private corService: CoresService, private clientesService: ClientesService,
+               private modeloService: ModeloService) { }
 
   ngOnInit() {
+    this.corService.getCores()
+      .subscribe( data => {
+        this.cores = data;
+      });
+
+    this.modeloService.getModelos()
+      .subscribe( data => {
+        this.modelos = data;
+      });
+
+    this.clientesService.getClientes()
+      .subscribe( data => {
+        this.clientes = data;
+      });
+  }
+
+  onSubmit() {
+    this.veiculoService.create(this.veiculo)
+      .subscribe( data => {
+        this.router.navigate(['/veiculos']);
+      });
   }
 
 }
--- src/app/editar-veiculo/editar-veiculo.component.ts
+++ src/app/editar-veiculo/editar-veiculo.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {Vaga} from '../models/vaga';
+import {Router} from '@angular/router';
+import {VeiculosService} from '../client/veiculos/service/veiculos.service';
+import {Veiculo} from '../models/veiculo';
+import {Cliente} from '../models/cliente';
+import {Modelo} from '../models/modelo';
+import {Cor} from '../models/cor';
+import {CoresService} from '../client/cores/service/cores.service';
+import {ClientesService} from '../client/clientes/service/clientes.service';
+import {ModeloService} from '../client/modelos/service/modelo.service';
 
 @Component({
   selector: 'app-editar-veiculo',
@@ -6,10 +16,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-veiculo.component.css']
 })
 export class EditarVeiculoComponent implements OnInit {
+  veiculos: Veiculo[];
+  cores: Cor[];
+  modelos: Modelo[];
+  clientes: Cliente[];
+  veiculoId: any;
 
-  constructor() { }
+  veiculo: Veiculo = {
+    id: null,
+    placa: null,
+    cliente: null,
+    modelo: null,
+    cor: null,
+    estacionadas: null
+  };
+
+  constructor( private router: Router, private veiculoService: VeiculosService,
+               private corService: CoresService, private clientesService: ClientesService,
+               private modeloService: ModeloService) { }
 
   ngOnInit() {
+    this.getVeiculos();
+
+    this.veiculoId = window.localStorage.getItem('editVeiculoId');
+    if (!this.veiculoId) {
+      alert('Ação Inválida.');
+      this.router.navigate(['vagas']);
+      return;
+    }
+
+    this.veiculoService.getVeiculoById(+this.veiculoId)
+      .subscribe( data => {
+        this.veiculo = data;
+      });
+
+    this.corService.getCores()
+      .subscribe( data => {
+        this.cores = data;
+      });
+
+    this.modeloService.getModelos()
+      .subscribe( data => {
+        this.modelos = data;
+      });
+
+    this.clientesService.getClientes()
+      .subscribe( data => {
+        this.clientes = data;
+      });
   }
 
+  private getVeiculos(): void {
+    this.veiculoService.getVeiculos().subscribe(data => {
+      this.veiculos = data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onSubmit() {
+    this.veiculoService.update(this.veiculo)
+      .subscribe( data => {
+        this.router.navigate(['/veiculos']);
+      });
+  }
 }
